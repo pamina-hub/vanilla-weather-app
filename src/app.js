6d462cb11dd92aaafc056c64b1fa6cbfa77c9476
pamina-hub <83540089+pamina-hub@users.noreply.github.com>
2021-07-18 18:14:01 +0200

    Simplified the CodeStrucutre of JS

--- src/app.js
+++ src/app.js
@@ -1,39 +1,70 @@
 let apiKey = "2c4a14026882da503eb13e803b24b4dd";
-///current search
-function showWeather(response) {
-  let updateCity = document.querySelector("#current-city");
-  let temperature = Math.round(response.data.main.temp);
-  let updateTemp = document.querySelector(".temperature");
-  updateCity.innerHTML = `${response.data.name}`;
-  updateTemp.innerHTML = `${temperature}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
-function retrievePosition(position) {
-  let apiKey = "2c4a14026882da503eb13e803b24b4dd";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
-}
-navigator.geolocation.getCurrentPosition(retrievePosition);
-let currentButton = document.querySelector("#currentLocationButton");
-currentButton.addEventListener("click", retrievePosition);
-///city search
-function search(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#search-location-input");
-  let h5 = document.querySelector("#current-city");
-  let units = "metric";
-  let apiKey = "2c4a14026882da503eb13e803b24b4dd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
-  if (cityInput.value) {
-    h5.innerHTML = `${cityInput.value}`;
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hours}:${minute}`;
 }
-function showTemp(response) {
+
+function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
-  let newTemp = document.querySelector(".temperature");
-  newTemp.innerHTML = `${temperature}`;
+  let currentTemperature = document.querySelector(".temperature");
+  let location = response.data.name;
+  let currentLocation = document.querySelector(".city");
+  let currentEmojiElement = document.querySelector("#currentEmoji");
+  let dateElement = document.querySelector(".date");
+
+  celsiusTemperature = Math.round(response.data.main.temp);
+
+  currentTemperature.innerHTML = `${temperature}`;
+  currentLocation.innerHTML = `${location}`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  currentEmojiElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentEmojiElement.setAttribute(
+    "alt",
+    `${response.data.weather[0].description}`
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
-let form = document.querySelector("#searchButton");
-form.addEventListener("click", search);
+///city options
+function displayParis(event) {
+  event.preventDefault();
+  let apiKey = "2c4a14026882da503eb13e803b24b4dd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Paris&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+let parisElement = document.querySelector("#paris");
+parisElement.addEventListener("click", displayParis);
